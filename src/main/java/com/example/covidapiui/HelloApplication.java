3624cb15eec8f2081adbbc34e39ea25725db6cd7
stepkos <stepkos@wp.pl>
2021-11-02 13:05:49 +0100

    start project interface

--- src/main/java/com/example/covidapiui/HelloApplication.java
+++ src/main/java/com/example/covidapiui/HelloApplication.java
@@ -1,8 +1,15 @@
 package com.example.covidapiui;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +17,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        Text dateText = new Text("Podaj date");
+        DatePicker datePicker = new DatePicker();
+
+        Text localizationText = new Text("Choose localization");
+        ChoiceBox localization = new ChoiceBox();
+        localization.getItems().addAll("Caly swiat", "Polska", "USA", "Rosja", "Niemcy");
+
     }
 
     public static void main(String[] args) {
